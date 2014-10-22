c945d1b012c7eac2ced58d30aca743160f827322
abdielou <abdiel.aviles@gmail.com>
2014-10-22 15:36:05 +0000

    refactoring

--- src/com/mozartanalytics/tutorials/nlp/sentiment/SentimentClassifier.groovy
+++ src/com/mozartanalytics/tutorials/nlp/sentiment/SentimentClassifier.groovy
@@ -1,71 +1,76 @@
 /**
  * Created by aviles on 10/20/2014.
+ * Based on : http://alias-i.com/lingpipe/demos/tutorial/sentiment/read-me.html
  */
-package com.mozartanalytics.tutorials.nlp.sentiment
 
 import com.aliasi.classify.Classification
 import com.aliasi.classify.Classified
 import com.aliasi.classify.DynamicLMClassifier
 
-/*****************
- * START
- */
-println "Training Polarity Model"
-// Data
+println "Loading training and testing data."
 def trainingTweets = loadTrainingData()
-def testTweets = loadTestData()
-// Classifier
+def testingTweets = loadTestData()
+
+println "Create a classifier for 'positive' and 'negative' categories with ngram of 8."
 String[] categories = [ "positive", "negative" ]
 int nGram = 8
 def classifier = DynamicLMClassifier.createNGramProcess(categories, nGram)
-// Train - Test
+
+println "Training the classifier model."
 train(classifier, categories, trainingTweets)
-evaluate(classifier, testTweets)
+
+println "Testing the classifier model."
+test(classifier, testingTweets)
 
 /*****************
  * Helper Methods
  */
 def train(classifier, categories, tweets) throws IOException {
-    println "Training."
-    def numTrainingCases = 0
-    def numTrainingChars = 0
+    // Informative variables
+    def (trainingCases, trainingChars) = [0,0]
 
+    // Classes (the NLP slang for categories)
     def classifications = [:]
     categories.each{ cat ->  classifications.put(cat, new Classification(cat)) }
 
+    // Feed the classifier with each classified tweet
     tweets.each{ tweet ->
-        ++numTrainingCases
-        numTrainingChars += tweet.text.length()
-
+        ++trainingCases
+        trainingChars += tweet.text.length()
+        // Manually Classify
         Classified<CharSequence> classified = new Classified<CharSequence>(tweet.text,classifications[tweet.category])
+        // Train
         classifier.handle(classified)
     }
 
-    println "  # Training Cases=" + numTrainingCases
-    println "  # Training Chars=" + numTrainingChars
+    println "  Training Cases : " + trainingCases
+    println "  Training Chars : " + trainingChars
 }
 
-def evaluate(classifier, tweets) {
-    println "Evaluating."
-    int numTests = 0
-    int numCorrect = 0
+def test(classifier, tweets) {
+    def (testingCases, numCorrect) = [0,0]
 
     tweets.each{ tweet ->
-        ++numTests
+        ++testingCases
+        // Predict Class
         Classification classification = classifier.classify(tweet.text)
         if (classification.bestCategory().equals(tweet.category))
             ++numCorrect
     }
 
-    println "  # Test Cases=" + numTests
-    println "  # Correct=" + numCorrect
-    println "  % Correct=" + ((double)numCorrect)/(double)numTests
+    println "  Testing Cases  : " + testingCases
+    println "  Correct #      : " + numCorrect
+    println "          %      : " + ((double)numCorrect)/(double)testingCases
 }
 
+/**
+ * The training and testing data must be classified 'a priory'. This is usually done manually or from an existing classified (categorized) corpus.
+ * In our code we expect the following object structure
+ * { text: "some text", category: "positive" }
+ */
 def loadTrainingData(){
     []
 }
-
 def loadTestData(){
     []
 }
